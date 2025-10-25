7d2700edf57650a44d39f27873d56f1221ab7b31
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2025-10-26 00:57:54 +0500

    init()

---
+++ README.md
@@ -0,0 +1,26 @@
+# Markdown Table of Contents
+
+A Sublime Text plugin that shows a Table of Contents for Markdown files with clickable entries.
+
+![](./docs/example.png)
+
+## Installation
+Clone the repository under your Packages directory (`~/Library/Application Support/Sublime Text/Packages`, `%APPDATA%\Sublime Text\Packages` or `~/.config/sublime-text/Packages`).  
+(The plugin files should be located in `Packages/mdtoc` directory)
+
+## Usage
+Press `Ctrl+Shift+M` to toggle the Table of Contents or use "Markdown Table of Contents: toggle" Command Palette entry.
+
+### Settings
+See the [default settings file](mdtoc.sublime-settings) for available settings and descriptions.
+
+## License
+This project is licensed under the [WTFPL License](http://www.wtfpl.net/):
+> Copyright © 2016 Xiangrong Hao
+>
+> Everyone is permitted to copy and distribute verbatim or modified  copies of this license document, and changing it is allowed as long as the name is changed.
+>
+> DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE  
+> TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION  
+>
+> 0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ mdtoc.py
@@ -0,0 +1,248 @@
+import html
+import re
+
+import sublime
+import sublime_plugin
+
+
+class Heading:
+    def __init__(self, title, level, position):
+        self.title = title
+        self.level = level
+        self.position = position
+
+
+phantoms = {}
+phantom_positions = {}
+
+
+class MdTocToggleCommand(sublime_plugin.TextCommand):
+    def run(self, _):
+        view = self.view
+        if not is_markdown_file(view):
+            sublime.status_message("This command only works with Markdown files")
+            return
+
+        view_id = view.id()
+        if view_id in phantoms and len(phantoms[view_id].phantoms) > 0:
+            hide_phantom(view)
+        else:
+            headings = parse_headings(view)
+            if not headings:
+                sublime.status_message("No headings found")
+                return
+            show_phantom(view, headings)
+
+
+class MdTocNavigateCommand(sublime_plugin.TextCommand):
+    def run(self, _, point):
+        view = self.view
+
+        view.sel().clear()
+        view.sel().add(sublime.Region(point, point))
+
+        view.show(point, False)
+        # 🩼 waiting for layout to be updated
+        sublime.set_timeout(lambda: view.show_at_center(point), 10)
+
+
+class MdTocListener(sublime_plugin.EventListener):
+    def on_load_async(self, view):
+        settings = sublime.load_settings("mdtoc.sublime-settings")
+        show_on_open = settings.get("show_on_open")
+
+        if not show_on_open:
+            return
+
+        if is_markdown_file(view):
+            headings = parse_headings(view)
+            if headings:
+                show_phantom(view, headings)
+
+    def on_modified_async(self, view):
+        # TODO: the function is kinda laggy on really large files
+        settings = sublime.load_settings("mdtoc.sublime-settings")
+        auto_update = settings.get("auto_update")
+
+        if not auto_update:
+            return
+
+        view_id = view.id()
+
+        if view_id in phantoms and len(phantoms[view_id].phantoms) > 0:
+            if is_markdown_file(view):
+                headings = parse_headings(view)
+                if headings:
+                    show_phantom(view, headings, is_reposition_needed=False)
+                else:
+                    hide_phantom(view)
+
+    def on_selection_modified_async(self, view):
+        settings = sublime.load_settings("mdtoc.sublime-settings")
+        hide_toc = settings.get("hide_toc")
+
+        if hide_toc == "after_cursor_move":
+            view_id = view.id()
+            if view_id in phantoms and len(phantoms[view_id].phantoms) > 0:
+                hide_phantom(view)
+
+    def on_close(self, view):
+        view_id = view.id()
+        if view_id in phantoms:
+            del phantoms[view_id]
+        if view_id in phantom_positions:
+            del phantom_positions[view_id]
+
+
+def parse_headings(view):
+    # the function doesn't handle ```code blocks``` correctly - according to the specification, all headers
+    # inside code blocks should be ignored (but nobody cares, lol)
+    headings = []
+
+    atx_pattern = r'^(?:>\s*)?(#{1,6})\s+(.+)$'
+    #                   ^ Blockquotes can contain other Markdown elements, including headers
+    setext_h1_pattern = r'^=+\s*$'
+    setext_h2_pattern = r'^-+\s*$'
+
+    ignored_lines = []
+
+    def get_setext_heading(setext_region, level):
+        row, _ = view.rowcol(setext_region.begin())
+        if row > 0 and row - 1 not in ignored_lines:
+            line_region = view.line(view.text_point(row - 1, 0))
+            ignored_lines.append(row)
+            # a line can't be both a text and directive - we need to skip it when processing
+            # the next line (it might be a setext header too)
+            line_content = view.substr(line_region)
+            if line_content.strip():
+                # if not, the next line should be treated as a horizontal rule, not a heading
+                return Heading(line_content, level, line_region.begin())
+        return None
+
+    for region in view.find_all(atx_pattern + "|" + setext_h1_pattern + "|" + setext_h2_pattern):
+        text = view.substr(region)
+        match = re.search(atx_pattern, text)
+        if match:
+            headings.append(Heading(match.group(2), len(match.group(1)), region.begin()))
+        match = re.search(setext_h1_pattern, text)
+        if match:
+            headings.append(get_setext_heading(region, 1))
+        match = re.search(setext_h2_pattern, text)
+        if match:
+            headings.append(get_setext_heading(region, 2))
+
+    return list(filter(None, headings))
+
+
+def is_markdown_file(view):
+    syntax = view.settings().get('syntax', '')
+    file_name = view.file_name() or ''
+    return 'markdown' in syntax.lower() or file_name.endswith(('.md', '.markdown'))
+
+
+def generate_toc_html(_, headings):
+    settings = sublime.load_settings("mdtoc.sublime-settings")
+    indent_width = settings.get("indent_width")
+
+    html_parts = []
+
+    link_color = 'var(--foreground)' if settings.get("link_color") == "text" else 'var(--bluish)'
+    dynamic_style = '''
+            .toc-link {{
+                text-decoration: none;
+                color: {color};
+            }}'''.format(color=link_color)
+
+    html_parts.append('''
+        <style>
+            .toc-container {{
+                border: 1px solid color(var(--foreground) alpha(0.5));
+                border-radius: 0.5em;
+                padding: 0.75em 0.75em;
+                margin: 0.75em 1em 0.75em 0;
+            }}
+            .toc-title {{
+                font-weight: bold;
+                color: var(--foreground);
+                padding-bottom: 0.5em;
+                border-bottom: 1px solid color(var(--foreground) alpha(0.5));
+                margin-bottom: 0.5em;
+            }}
+            .toc-indent {{
+                font-family: monospace;
+                white-space: pre-wrap;
+            }}
+            {dynamic_style}
+        </style>
+    '''.format(dynamic_style=dynamic_style))
+    html_parts.append(
+        '<div class="toc-container"><div class="toc-title">Table of Contents</div><div class="toc-content">')
+
+    for index, heading in enumerate(headings):
+        html_parts.append('<span class="indent">{}</span><a class="toc-link" href="{}">{}</a><br/>'.format(
+            "&nbsp;" * max(heading.level - 1, 0) * indent_width, heading.position, html.escape(heading.title)))
+
+    html_parts.append('</div></div></div>')
+
+    return ''.join(html_parts)
+
+
+def calc_phantom_position(view, headings):
+    settings = sublime.load_settings("mdtoc.sublime-settings")
+    position = settings.get("toc_position")
+
+    if position == "document_end":
+        return view.size()
+    elif position == "after_first_heading" and len(headings) > 0:
+        first_heading = headings[0]
+        line_region = view.line(first_heading.position)
+        return line_region.end()
+    elif position == "at_cursor":
+        return view.sel()[0].begin() if len(view.sel()) > 0 else 0
+    else:
+        return 0
+
+
+def show_phantom(view, headings, is_reposition_needed=True):
+    view_id = view.id()
+
+    if view_id not in phantoms:
+        phantoms[view_id] = sublime.PhantomSet(view, 'md_toc')
+    phantom_set = phantoms[view_id]
+
+    html_content = generate_toc_html(view, headings)
+    if is_reposition_needed or view_id not in phantom_positions:
+        position = calc_phantom_position(view, headings)
+        phantom_positions[view_id] = position
+    else:
+        position = phantom_positions[view_id]
+
+    phantom = sublime.Phantom(
+        sublime.Region(position, position),
+        html_content,
+        sublime.LAYOUT_BLOCK,
+        on_navigate=lambda href: handle_toc_click(view, href)
+    )
+    phantom_set.update([phantom])
+
+
+def hide_phantom(view):
+    view_id = view.id()
+    if view_id in phantoms:
+        phantoms[view_id].update([])
+    if view_id in phantom_positions:
+        del phantom_positions[view_id]
+
+
+def handle_toc_click(view, href):
+    settings = sublime.load_settings("mdtoc.sublime-settings")
+    hide_toc = settings.get("hide_toc")
+
+    try:
+        point = int(href)
+        view.run_command('md_toc_navigate', {'point': point})
+
+        if hide_toc != "never":
+            hide_phantom(view)
+    except ValueError:
+        pass  # ¯\_(ツ)_/¯
